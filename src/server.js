3f2fdd39dff16568b653da390fcb619cc350c61e
Volodimir <161057867+volodimirfushtei@users.noreply.github.com>
2024-11-12 14:28:51 +0200

    add config

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+const PORT = 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello world!',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
