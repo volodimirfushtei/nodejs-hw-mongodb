ce0048c94143daf82ac387a923006fca7b600751
Volodimir <161057867+volodimirfushtei@users.noreply.github.com>
2024-12-22 00:13:51 +0200

    add

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -76,7 +76,11 @@ export const requestResetEmailController = async (req, res) => {
   const { email } = req.body;
   console.log({ email });
   requestResetPassword(email);
-  res.send({ status: 200, message: 'Reset Email has been sent', data: {} });
+  res.send({
+    status: 200,
+    message: 'Reset password email has been successfully sent.',
+    data: {},
+  });
 };
 export async function requestResetPasswordController(req, res) {
   const { password, token } = req.body;
