52672ae9634fb3fa4f038bb6fa8827585b4a5f48
Volodimir <161057867+volodimirfushtei@users.noreply.github.com>
2024-12-29 13:03:47 +0200

    add

--- src/utils/parseFilterParams.js
+++ src/utils/parseFilterParams.js
@@ -1,18 +1,6 @@
 function parseFilterParams(query) {
   const filters = {};
 
-  if (query.name) {
-    filters.name = { $regex: query.name, $options: 'i' };
-  }
-
-  if (query.phoneNumber) {
-    filters.phoneNumber = query.phoneNumber;
-  }
-
-  if (query.email) {
-    filters.email = query.email;
-  }
-
   if (query.isFavourite) {
     filters.isFavourite = query.isFavourite === 'true';
   }
